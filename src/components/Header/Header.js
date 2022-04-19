b97099e65aa7aa3c02b15516b81af68c343e9e91
Raphaelhngstn <raphael.honig@gmail.com>
2022-04-19 11:02:43 +0200

    modal hamburger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,12 @@ import './Header.scss';
 import React from 'react';
 import logo from '../../images/logo.svg';
 import { useNavigate } from 'react-router-dom';
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
 
     const navigate = useNavigate();
 
@@ -11,19 +15,50 @@ const Header = () => {
         navigate("/");
     }
 
-    return (
-    <div className="header">
-        <img onClick={handleLogo} className="logo" src={logo} alt='logo'/>
-        <div className="header-links">
-            <a className="header-link" href="/about"><span className='header-green'>A</span>bout</a>
-            <a className="header-link" href="/skills"><span className='header-green'>S</span>kills</a>
-            <a className="header-link" href="/formation"><span className='header-green'>F</span>ormations</a>
-            <a className="header-link" href="/formation"><span className='header-green'>P</span>ortfolio</a>
-            <a className="header-link" href="/contact"><span className='header-green'>C</span>ontact</a>
-        </div>
-        
-    </div>
+    const handleModal = () => {
+        setIsOpen(oldState => !oldState);
+    }
 
+    return (
+    <>
+        
+        <div className="header">
+        
+            <img onClick={handleLogo} className="logo" src={logo} alt='logo'/>
+            <div className="header-links">
+                <a className="header-link" href="/about"><span className='header-green'>A</span>bout</a>
+                <a className="header-link" href="/skills"><span className='header-green'>S</span>kills</a>
+                <a className="header-link" href="/formation"><span className='header-green'>F</span>ormations</a>
+                <a className="header-link" href="/formation"><span className='header-green'>P</span>ortfolio</a>
+                <a className="header-link" href="/contact"><span className='header-green'>C</span>ontact</a>
+            </div>
+
+            <div className="header-hamburger">
+            
+                <Hamburger
+                    rounded
+                    duration={0.6}
+                    size={48}
+                    distance="sm"
+                    onToggle={handleModal}
+                />
+             
+             </div>
+
+             {
+                 isOpen && 
+                 <div className="header-links-mobile">
+                     <a className="header-link-mobile" href="/"><span className='header-green'>H</span>ome</a>
+                    <a className="header-link-mobile" href="/about"><span className='header-green'>A</span>bout</a>
+                    <a className="header-link-mobile" href="/skills"><span className='header-green'>S</span>kills</a>
+                    <a className="header-link-mobile" href="/formation"><span className='header-green'>F</span>ormations</a>
+                    <a className="header-link-mobile" href="/formation"><span className='header-green'>P</span>ortfolio</a>
+                    <a className="header-link-mobile" href="/contact"><span className='header-green'>C</span>ontact</a>
+                </div>
+             }
+        
+        </div>
+    </>
     )
 };
 
